d9064f7fbdf2b7ab55d9807ae526a9af2413c448
Lukas Wenzel <lukas.wenzel@student.hpi.de>
2019-01-11 19:10:11 +0100

    [WIP] Define Pipeline Structure of ExtentStore

--- actions/hdl_fosix/hw/ExtentStore.vhd
+++ actions/hdl_fosix/hw/ExtentStore.vhd
@@ -61,36 +61,132 @@ architecture ExtentStore of ExtentStore is
   constant c_LRowAddrWidth : integer := 4;
   subtype t_LRowAddr is unsigned (c_LRowAddrWidth-1 downto 0);
 
-  constant c_LRowEntryAddrWidth : integer := 4;
-  subtype t_LRowEntryAddr is unsigned (c_LRowEntryAddrWidth-1 downto 0);
+  constant c_LColAddrWidth : integer := 4;
+  constant c_LColCount : integer := 2**c_LColAddrWidth;
+  subtype t_LColAddr is unsigned (c_LColAddrWidth-1 downto 0);
 
-  constant c_EntryAddrWidth : integer := c_RowAddrWidth + c_RowEntryAddrWidth;
-  subtype t_EntryAddr is unsigned (c_EntryAddrWidth-1 downto 0);
+  constant c_PBlkAddrWidth : integer := c_LRowAddrWidth + c_LColAddrWidth;
+  subtype t_PBlkAddr is unsigned (c_PBlkAddrWidth-1 downto 0);
 
-  constant c_LRowWidth : integer := (2**c_LRowEntryAddrWidth) * c_LBlkWidth;
+  constant c_LRowWidth : integer := c_LColCount * c_LBlkWidth;
   subtype t_LRow is unsigned (c_LRowWidth-1 downto 0);
 
   signal s_lstoreWrAddr : t_EntryAddr;
   signal s_lstoreWrData : t_LBlk;
   signal s_lstoreWrEn   : std_logic;
-  signal s_lstoreRdAddr : t_LRowAddr;
-  signal s_lstoreRdData : t_LRow;
-
   signal s_pstoreWrAddr : t_EntryAddr;
   signal s_pstoreWrData : t_PBlk;
   signal s_pstoreWrEn   : std_logic;
-  signal s_pstoreRdAddr : t_EntryAddr;
-  signal s_pstoreRdData : t_PBlk;
+
+  signal s_lrowAddr_0   : t_LRowAddr;
+  signal s_reqLblk_0   : t_LRowAddr;
+  signal s_reqPort_0   : t_LRowAddr;
+  signal s_enable_0   : t_LRowAddr;
+
+  signal s_lrowAddr_1   : t_LRowAddr;
+  signal s_reqLblk_1   : t_LRowAddr;
+  signal s_reqPort_1   : t_LRowAddr;
+  signal s_enable_1   : t_LRowAddr;
+
+  signal s_lrowAddr_2   : t_LRowAddr;
+  signal s_lrow_2       : t_LRow;
+  signal s_reqLBlk_2   : t_LRowAddr;
+
+  type t_RowLBlks is array (integer range <>) of t_LBlk;
+  signal s_extLBlks_2 : t_RowLBlks(c_LColCount-2 downto 0);
+  signal s_extLCnts_2 : t_RowLBlks(c_LColCount-2 downto 0);
+  signal s_extValids_2 : unsigned(c_LColCount-2 downto 0);
+
+  signal s_lcolAddr_2   : t_LRowAddr;
+  signal s_extLBlk_2   : t_LRowAddr;
+  signal s_extLCnt_2   : t_LRowAddr;
+  signal s_extValid_2   : t_LRowAddr;
+  signal s_reqPort_2   : t_LRowAddr;
+  signal s_enable_2   : t_LRowAddr;
+  signal s_pblkAddr_2   : t_PBlkAddr;
+
+  signal s_extLBlk_3   : t_LRowAddr;
+  signal s_extLCnt_3   : t_LRowAddr;
+  signal s_extValid_3   : t_LRowAddr;
+  signal s_reqPort_3   : t_LRowAddr;
+  signal s_enable_3   : t_LRowAddr;
+
+  signal s_pblk_4       : t_PBlk;
+  signal s_extLBlk_4   : t_LRowAddr;
+  signal s_extLCnt_4   : t_LRowAddr;
+  signal s_extValid_4   : t_LRowAddr;
+  signal s_reqPort_4   : t_LRowAddr;
+  signal s_enable_4   : t_LRowAddr;
 
 begin
 
+  -- Matching Logic (Stage 2)
+  process(s_lrow_2, s_reqLBlk_2)
+    variable v_thisCol : t_LBlk;
+    variable v_nextCol : t_LBlk;
+  begin
+    for v_index in 0 to c_ColCount-2 loop
+      v_thisLCol := f_resize(s_lrow_2, c_LBlkWidth, v_index * c_LBlkWidth);
+      v_nextLCol := f_resize(s_lrow_2, c_LBlkWidth, (v_index+1) * c_LBlkWidth);
+      s_extValids_2(v_index)
+      if v_thisCol <= s_reqLBlk_2 and s_reqLBlk_2 < v_nextCol then
+        --TODO-lw continue
+      end if;
+    end loop;
+  end process;
 
-  -- Logical Block Offset Store Instantiation
+
+  -----------------------------------------------------------------------------
+  -- Pipeline Registers
+  -----------------------------------------------------------------------------
+
+  process(pi_clk)
+  begin
+    if pi_clk'event and pi_clk = '1' then
+      if pi_rst_n = '0' then
+        s_enable_1   <= '0';
+        s_enable_2   <= '0';
+        s_enable_3   <= '0';
+        s_extValid_3 <= '0';
+        s_enable_4   <= '0';
+        s_extValid_4 <= '0';
+      else
+        s_lrowAddr_1 <= s_lrowAddr_0;
+        s_reqLblk_1  <= s_reqLblk_0;
+        s_reqPort_1  <= s_reqPort_0;
+        s_enable_1   <= s_enable_0;
+
+        s_lrowAddr_2 <= s_lrowAddr_1;
+        s_reqLBlk_2  <= s_reqLblk_1;
+        s_reqPort_2  <= s_reqPort_1;
+        s_enable_2   <= s_enable_1;
+
+        s_extLBlk_3  <= s_extLBlk_2;
+        s_extLCnt_3  <= s_extLCnt_2;
+        s_extValid_3 <= s_extValid_2;
+        s_reqPort_3  <= s_reqPort_2;
+        s_enable_3   <= s_enable_2;
+
+        s_extLBlk_4  <= s_extLBlk_3;
+        s_extLCnt_4  <= s_extLCnt_3;
+        s_extValid_4 <= s_extValid_3;
+        s_reqPort_4  <= s_reqPort_3;
+        s_enable_4   <= s_enable_3;
+      end if;
+    end if;
+  end process;
+
+
+  -----------------------------------------------------------------------------
+  -- BRAM Instatiations
+  -----------------------------------------------------------------------------
+
+  -- Logical Block Store
   s_lstoreWrAddr_v <= std_logic_vector(s_lstoreWrAddr);
   s_lstoreWrData_v <= std_logic_vector(s_lstoreWrData);
   s_lstoreWrEn_v   <= std_logic_vector(s_lstoreWrEn);
-  s_lstoreRdAddr_v <= std_logic_vector(s_lstoreRdAddr);
-  s_lstoreRdData   <= t_LRow(s_lstoreRdData_v);
+  s_lstoreRdAddr_v <= std_logic_vector(s_lrowAddr_0);
+  s_lrow_2 <= t_LRow(s_lstoreRdData_v);
   i_lstore : bram_w256x32r16x512
     port map(
       clka  => pi_clk,
@@ -101,12 +197,12 @@ begin
       addrb => s_lstoreRdAddr_v,
       doutb => s_lstoreRdData_v);
 
-  -- Physical Block Store Instantiation
+  -- Physical Block Store
   s_pstoreWrAddr_v <= std_logic_vector(s_pstoreWrAddr);
   s_pstoreWrData_v <= std_logic_vector(s_pstoreWrData);
   s_pstoreWrEn_v   <= std_logic_vector(s_pstoreWrEn);
-  s_pstoreRdAddr_v <= std_logic_vector(s_pstoreRdAddr);
-  s_pstoreRdData   <= t_LRow(s_pstoreRdData_v);
+  s_pstoreRdAddr_v <= std_logic_vector(s_pblkAddr_2);
+  s_pblk_4 <= t_LRow(s_pstoreRdData_v);
   i_pstore : bram_w256x64r256x64
     port map(
       clka  => pi_clk,
